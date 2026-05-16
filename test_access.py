8771b6ef0745fbc37af5b62044cbe99de5398c81
Nikolay Petukhov <kol.ride.pro.00@gmail.com>
2026-05-16 05:53:56 +0500

    Create test_access.py

--- test_access.py
+++ test_access.py
@@ -1,1 +1,17 @@
+import pytest
+import time
+from main import param_for_test
 
+
+def test_wait_timer():
+    """
+    Пауза
+    """
+    time.sleep(15)
+    assert 1 == 1
+
+def test_work():
+    """
+    Пауза
+    """
+    assert param_for_test == 100
